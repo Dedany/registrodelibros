3d0560f3b83d9fc2629d9bb7aeb18b5f8790e6cc
Daniel Pérez Álvarez <danylerez@gmail.com>
2025-07-28 11:07:41 +0200

    Add Retrofit

---
+++ app/src/main/java/com/dedany/registrodelibros/di/NetworkModule.kt
@@ -0,0 +1,44 @@
+package com.dedany.registrodelibros.di
+
+import com.dperez.data.datasource.remote.service.OpenLibraryApi
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+object NetworkModule {
+    private const val BASE_URL = "https://openlibrary.org/"
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().create()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient, gson: Gson): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOpenLibraryApi(retrofit: Retrofit): OpenLibraryApi =
+        retrofit.create(OpenLibraryApi::class.java)
+}
